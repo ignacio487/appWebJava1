9c2cd9e177c9057c3dfbd7bdb05e8949382c21be
ignacio487 <i.riquelme10@ufromail.cl>
2023-11-09 23:52:29 -0300

    Modificaciones con errores a corregir

--- src/main/java/com/registro/registropersonas/model/Usuario.java
+++ src/main/java/com/registro/registropersonas/model/Usuario.java
@@ -1,23 +1,49 @@
 package com.registro.registropersonas.model;
 
-public class Usuario {
-    private String nombre;
-    private int edad;
+import com.registro.registropersonas.model.data.DBConnector;
+import com.registro.registropersonas.model.data.dao.UsuarioDAO;
+import org.jooq.DSLContext;
+import org.jooq.impl.DSL;
 
-    public Usuario (String nombre, int edad){
-        this.nombre=nombre;
-        this.edad=edad;
+import java.sql.Connection;
+
+public class Usuario {
+    String nombre;
+    int edad;
+    String rut;
+    public Usuario(String nombre, int edad, String rut) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.rut = rut;
     }
-    public String getNombre(){
+
+    public String getNombre() {
         return nombre;
     }
-    public void setNombre(){
-        this.nombre=nombre;
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
-    public int getEdad(){
+
+    public int getEdad() {
         return edad;
     }
-    public void setEdad(){
-        this.edad=edad;
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+    public boolean registrarUsuario(){
+        Connection connection= DBConnector.connection("usuario","root","");
+        DSLContext query= DSL.using(connection);
+        System.out.println("a");
+        return new UsuarioDAO().registrarUsuario(query,this);
     }
 }
